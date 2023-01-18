37d8f8ffe452701265d7c88cc20c45842718b938
Niklas Lorenz <niklaslorenz1999@gmail.com>
2023-01-18 18:42:09 +0100

    Fixed an indexing bug in util.full_lane_index

--- sha3-icore/src/util.vhdl
+++ sha3-icore/src/util.vhdl
@@ -7,6 +7,8 @@ package util is
 
     function to_lane(vec : std_ulogic_vector) return lane_t;
 
+    function full_lane_index(x : natural range 0 to 4; y : natural range 0 to 4) return full_lane_index_t;
+
 end package;
 
 package body util is
@@ -14,6 +16,11 @@ package body util is
     function to_lane(vec : std_ulogic_vector) return lane_t is
     begin
         return lane_t(vec);
+    end function;
+
+    function full_lane_index(x : natural range 0 to 4; y : natural range 0 to 4) return full_lane_index_t is
+    begin
+        return y * 5 + x;
     end function;
 
 end package body;
