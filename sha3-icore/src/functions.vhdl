bf209423ed55a02a6f6eada8272a4a37913bddf3
Niklas Lorenz <niklaslorenz1999@gmail.com>
2023-01-18 18:43:38 +0100

    Added the theta functions and implemented the theta tests

---
+++ sha3-icore/src/functions.vhdl
@@ -0,0 +1,51 @@
+library IEEE;
+
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use work.state.all;
+use work.util.all;
+
+package slice_functions is
+
+    function theta_sums(slice : slice_t) return std_logic_vector;
+    function theta(lower_sums : std_logic_vector(4 downto 0); higher_sums : std_logic_vector(4 downto 0); slice : slice_t) return slice_t;
+    function theta(previous_slice : slice_t; this_slice : slice_t) return slice_t;
+
+end package slice_functions;
+
+package body slice_functions is
+
+    function theta_sums(slice : slice_t) return std_logic_vector is
+        variable column_sums : std_logic_vector(4 downto 0) := (others => '0');
+    begin
+        for x in 0 to 4 loop
+            for y in 0 to 4 loop
+                column_sums(x) := column_sums(x) xor slice(full_lane_index(x, y));
+            end loop;
+        end loop;
+        return column_sums;
+    end function;
+
+    function theta(lower_sums : std_logic_vector(4 downto 0); higher_sums : std_logic_vector(4 downto 0); slice : slice_t) return slice_t is
+        variable result : slice_t;
+        variable column_modifiers : std_logic_vector(4 downto 0) := (others => '0');
+    begin
+        for x in 0 to 4 loop
+            column_modifiers(x) := higher_sums((x + 4) mod 5) xor lower_sums((x + 1) mod 5);
+            for y in 0 to 4 loop
+                result(full_lane_index(x, y)) := slice(full_lane_index(x, y)) xor column_modifiers(x);
+            end loop;
+        end loop;
+        return result;
+    end function;
+
+    function theta(previous_slice : slice_t; this_slice : slice_t) return slice_t is
+        variable prev_temp : std_logic_vector(4 downto 0);
+        variable this_temp : std_logic_vector(4 downto 0);
+    begin
+        prev_temp := theta_sums(previous_slice);
+        this_temp := theta_sums(this_slice);
+        return theta(prev_temp, this_temp, this_slice);
+    end function;
+
+end package body;
---
+++ sha3-icore/test/theta_test.vhdl
@@ -0,0 +1,52 @@
+LIBRARY IEEE;
+
+use IEEE.std_logic_1164.all;
+use std.textio.all;
+use IEEE.numeric_std.all;
+
+use work.state.all;
+use work.util.all;
+use work.slice_functions.all;
+
+entity theta_test is
+end entity;
+
+architecture arch of theta_test is
+
+    constant zero : lane_t := (others => '0');
+
+    signal clk : std_logic := '0';
+    signal finished : boolean := false;
+    signal result : slice_t;
+    signal slice1_sums, slice2_sums : std_logic_vector(4 downto 0);
+
+begin
+
+    clk_process : process is
+    begin
+        while not finished loop
+            clk <= not clk;
+            wait for 5ns;
+        end loop;
+        wait;
+    end process;
+
+    test_process : process is
+        constant slice_1 : slice_t := "1101001001011111000101100";
+        constant slice_2 : slice_t := "1010101101001001111110101";
+        constant expected : slice_t := "0100010000110010001001000";
+    begin
+        wait for 2 ns;
+        wait until rising_edge(clk);
+        slice1_sums <= theta_sums(slice_1);
+        slice2_sums <= theta_sums(slice_2);
+        result <= theta(slice_1, slice_2);
+        wait until rising_edge(clk);
+        assert slice1_sums = "00001" report "Wrong theta sum for slice 1" severity FAILURE;
+        assert slice2_sums = "10110" report "Wrong theta sum for slice 2" severity FAILURE;
+        assert result = expected report "Wrong result for theta" severity FAILURE;
+        finished <= true;
+        wait;
+    end process;
+
+end architecture;
