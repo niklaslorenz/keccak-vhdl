a3d61bd8e035ae8e189fdd66c5ce5b57fc5da5c7
Niklas Lorenz <niklaslorenz1999@gmail.com>
2023-01-25 13:18:30 +0100

    Implemented the write method in reader

--- sha3-icore/src/modules/reader.vhdl
+++ sha3-icore/src/modules/reader.vhdl
@@ -10,6 +10,8 @@ package reader is
     
     procedure read(iterator : inout reader_t; state : inout block_t; lane : lane_t; atom_index : atom_index_t; ready : out std_logic);
 
+    procedure write(iterator : inout reader_t; state : in block_t; atom_index : atom_index_t; data_out : out lane_t; ready : out std_logic);
+
 end package;
 
 package body reader is
@@ -31,6 +33,19 @@ package body reader is
             end if;
         end if;
         if iterator = 16 then
+            ready := '1';
+        else
+            iterator := iterator + 1;
+            ready := '0';
+        end if;
+    end procedure;
+    
+    procedure write(iterator : inout reader_t; state : in block_t; atom_index : atom_index_t; data_out : out lane_t; ready : out std_logic) is
+    begin
+        if atom_index = 0 and iterator <= 3 then
+            data_out := state(iterator);
+        end if;
+        if iterator >= 4 then
             ready := '1';
         else
             iterator := iterator + 1;
