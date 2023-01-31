f5a2b7b8a1257240645032e3968177c82c43017d
Niklas Lorenz <niklaslorenz1999@gmail.com>
2023-01-31 11:47:38 +0100

    Fixed compiler errors in calculator

--- sha3-icore/src/modules/calculator.vhdl
+++ sha3-icore/src/modules/calculator.vhdl
@@ -11,7 +11,7 @@ entity calculator is
         no_theta : in std_logic;
         round_constant_bit : in std_logic;
         result : out slice_t;
-        slice_sums : out std_logic_vector(4 downto 0);
+        slice_sums : out std_logic_vector(4 downto 0)
     );
 end entity;
 
@@ -21,25 +21,17 @@ architecture arch of calculator is
     signal beta_result : slice_t;
     signal theta_input : slice_t;
     signal theta_result : slice_t;
-    signal theta_sums : std_logic_vector(4 downto 0);
+    signal slice_sums_temp : std_logic_vector(4 downto 0);
 begin
 
     pi_chi_result <= chi(pi(slice));
     beta_result <= pi_chi_result(24 downto 1) & (pi_chi_result(0) xor round_constant_bit);
 
-    theta_sums <= theta_sums(theta_input);
-    theta_result <= theta(prev_sums, theta_sums, theta_input);
-    slice_sums <= theta_sums;
+    slice_sums_temp <= theta_sums(theta_input);
+    theta_result <= theta(prev_sums, slice_sums_temp, theta_input);
+    slice_sums <= slice_sums_temp;
 
-    if theta_only = '1' then
-        theta_input <= slice;
-    else
-        theta_input <= beta_result;
-    end if;
+    theta_input <= slice when theta_only = '1' else beta_result;
+    result <= beta_result when no_theta = '1' else theta_result;
 
-    if no_theta = '1' then
-        result <= beta_result;
-    else
-        result <= theta_result;
-    end if;
 end architecture;
